d3d2ce7d929f837ad6c1207e30fdf64a718022ea
Jaffer-ali920 <abooka920@gmail.com>
2022-12-19 10:36:58 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kpmg-employees-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'kpmg-employees';
+}
